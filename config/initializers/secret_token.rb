e809614a17b656f23a340b8a4038a795df1cd6ab
Jonathan <jonathan@cardinalblue.com>
2013-04-12 20:11:50 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchFantunes::Application.config.secret_token = '9c3a9b4678942858dc19d6034aa1e918cd28a1563d667f18d5b725f85b8f4a787a41326946f429c0877fb7e1e1e8d4642d58e41a5071a6863187c56af3cf376b'

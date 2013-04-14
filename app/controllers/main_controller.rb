ea963ddd2ceab3cb4877b7167e70a1e5b6f0a4b9
Jonathan <jonathan@cardinalblue.com>
2013-04-14 17:50:29 +0800

    index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     if params[:sequence]
-      @boards = Board.find_auction params[:sequence]
+      @boards = Board.find_auction params[:sequence].upcase
     else
       @boards = []
     end

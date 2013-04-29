5e2e2f36b41fd17ccdf160684af49ec10ca35da6
Jonathan <jonathan@cardinalblue.com>
2013-04-29 11:47:42 +0800

    number of results

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
+    @sequence = params[:sequence]
     if params[:sequence]
       @boards = Board.find_auction params[:sequence].upcase
     else
